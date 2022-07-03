696b62df0f748a16bad942796d6f9d43592ffd8a
KAA83 <98401171+KAA83@users.noreply.github.com>
2022-07-03 21:42:07 +0500

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,29 @@
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr;
+  if(a === 0)
+        return false;
+  let arr = {};
+  let D = b ** 2 - 4 * a * c;
+    console.log('D = ' + D);
+    if(D < 0)
+        return false;
+    arr['discriminant'] = D;
+    if(D === 0)
+        arr["quadratic roots"] = (-b + Math.sqrt(D)) / (2 * a);
+    else if(D > 0){
+        let tmp = [];
+        tmp.push((-b + Math.sqrt(D)) / (2 * a));
+        tmp.push((-b - Math.sqrt(D)) / (2 * a));
+        arr["quadratic roots"] = tmp;
+    }
+    
   // код для задачи №1 писать здесь
   return arr; // array
 }
+
+let result = solveEquation(2, 16, 8);
+console.log(result);
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;

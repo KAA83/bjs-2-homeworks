0709ce9e8ad71dad1485fccec76af674dd6ddb4a
KAA83 <98401171+KAA83@users.noreply.github.com>
2022-07-07 12:38:26 +0500

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,16 +1,13 @@
 "use strict";
 
 function solveEquation(a, b, c) {
-  let D = b ** 2 - 4 * a * c; 
+  let d = b ** 2 - 4 * a * c; 
   let arr = [];
-  if(D > 0){
-    arr = [];
-    arr.push((-b + Math.sqrt(D)) / (2 * a));
-    arr.push((-b - Math.sqrt(D)) / (2 * a));
-  } else if(D === 0) {
+  if(d > 0){
+    arr.push((-b + Math.sqrt(d)) / (2 * a));
+    arr.push((-b - Math.sqrt(d)) / (2 * a));
+  } else if(d === 0) {
       arr = [-b / (2 * a)];
-  } else if(D < 0) {
-      arr = []; 
   }
 
   return arr; // array

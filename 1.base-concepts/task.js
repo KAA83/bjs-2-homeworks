984524775ac2281cb5b03832714e377f8c35eab2
Александр Колосов <rain_ne@mail.ru>
2022-10-07 20:52:01 +0500

    add functions

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,29 +1,18 @@
 "use strict";
 
 function solveEquation(a, b, c) {
-  if(a === 0)
-        return false;
-  let arr = {};
-  let D = b ** 2 - 4 * a * c;
-    console.log('D = ' + D);
-    if(D < 0)
-        return false;
-    arr['discriminant'] = D;
-    if(D === 0)
-        arr["quadratic roots"] = (-b + Math.sqrt(D)) / (2 * a);
-    else if(D > 0){
-        let tmp = [];
-        tmp.push((-b + Math.sqrt(D)) / (2 * a));
-        tmp.push((-b - Math.sqrt(D)) / (2 * a));
-        arr["quadratic roots"] = tmp;
-    }
-    
-  // код для задачи №1 писать здесь
+  let d = b ** 2 - 4 * a * c; 
+  let arr = [];
+  if(d > 0){
+    arr.push((-b + Math.sqrt(d)) / (2 * a));
+    arr.push((-b - Math.sqrt(d)) / (2 * a));
+  } else if(d === 0) {
+      arr = [-b / (2 * a)];
+  } 
+
   return arr; // array
 }
 
-let result = solveEquation(2, 16, 8);
-console.log(result);
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,52 @@
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
-
-  // Ваш код
-
-  return { min: min, max: max, avg: avg };
+  sum =0;
+  min = max = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    sum += arr[i];
+  }
+  avg = Number((sum / arr.length).toFixed(2));
+  return { min:min, max:max, avg:avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
+  let max = 0;
+  for (let i = 0; i < arrOfArr.length; i++) {
+    let arrSum = func(arrOfArr[i]);
+    if (arrSum > max) {
+      max = arrSum;
+    }
+  }
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min, max;
+  min = max = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+  return Math.abs(max - min);
 }
